4877e081e0a11cca3197cb2d3ff821c371b0720c
schnow265 <thesnowbox@icloud.com>
2024-12-16 14:49:18 +0100

    add all dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,36 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  },
+  nitro: {
+    prerender: {
+      autoSubfolderIndex: false
+    }
+  },
+
+  i18n: {
+    locales: [
+      { code: 'en', language: 'en-US' },
+    ],
+    defaultLocale: 'en'
+  },
+  modules: [
+    '@nuxtjs/i18n',
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt',
+    '@nuxtjs/color-mode',
+    '@nuxthub/core',
+  ],
 })
